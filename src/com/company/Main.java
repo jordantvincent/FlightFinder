93924a8e46b3cd962414b1d981886c74b278f987
Jordan Vincent <thelifeofjtv@gmail.com>
2019-10-10 21:58:12 -0400

    Setting Up Chrome Driver

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
 
+    private static WebDriver driver;
+
     public static void main(String[] args) {
-	// write your code here
+    //CHROME DRIVER SETUP
+
+    //Each of us will have a different file path so just comment out the other one while you work
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\theli\\Desktop\\chromedriver.exe"); // Jordan's File Path
+        //System.setProperty("webdriver.chrome.driver","C:\\Users\\Desktop\\chromedriver.exe"); // Ethan's File Path
+
+        driver = new ChromeDriver();
+
+        driver.get("http://www.google.com");
     }
+
 }
